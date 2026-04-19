b93d76a32a00b63a279fbb0897da673605ea6af8
tebibusolomonka-ops <tebibusolomonka-ops@users.noreply.github.com>
2026-04-19 12:16:24 +0300

    Fixed web category icons

--- src/pages/Buy.tsx
+++ src/pages/Buy.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { getCategories } from '../services/listing_service';
 import type { Category } from '../services/listing_service';
 import { CATEGORIES } from '../data/mockData';
 import {
